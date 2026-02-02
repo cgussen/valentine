f0ca58638c4a87ea7faae224cdd76b4627d0d703
Charles Gussen <cgussen@nd.edu>
2026-02-02 14:46:37 -0500

    Irrelevant change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ fasho
 export default defineConfig({
   base: "/valentine",
   plugins: [react()],
